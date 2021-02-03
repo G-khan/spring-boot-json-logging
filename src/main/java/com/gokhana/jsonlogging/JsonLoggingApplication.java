50dfc5cb9dea5a2cd1425634ae5df8d63697e425
Gökhan <gkhan.a.06@gmail.com>
2021-02-03 22:28:14 +0300

    initial commit

---
+++ src/main/java/com/gokhana/jsonlogging/JsonLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.gokhana.jsonlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonLoggingApplication.class, args);
+    }
+
+}
